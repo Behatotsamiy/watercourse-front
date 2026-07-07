f8563d7391204771d4a12b290dc822dc77be1510
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2026-07-07 13:05:04 +0500

    api

--- src/Shared/API/base.ts
+++ src/Shared/API/base.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://watercourse-back.onrender.com', // 👈 https + /api
+  baseURL: 'https://watercourse-back.onrender.com/api', // 👈 https + /api
   withCredentials: true, // 👈 для refreshToken cookie
 });
 
